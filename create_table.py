eaf8c9aac6d06f997b9cc1eb859e4631781f6d7d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-03 23:53:30 +0600

    create table

---
+++ create_table.py
@@ -0,0 +1,20 @@
+import mysql.connector
+db_name = 'python_test_db'
+mydbconnection = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd='123456',
+    database=db_name
+)
+
+mycursor = mydbconnection.cursor()
+
+sqlquery ="""
+            CREATE TABLE Student
+            (
+                Roll varchar(4),
+                Name varchar(50)
+            )
+           """
+mycursor.execute(sqlquery)
+print('Create table Successfull')
